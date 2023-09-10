fa8e938a19addaa913230aa0ceca3d72fd0f68fb
nachoco <lordnach14@gmail.com>
2023-09-10 20:56:47 -0300

    actualizacion

--- actualizadordemods.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo añadiendo mods
-git add .
-echo agregando comentario
-git commit -m "actualizacion"
-echo Realizando push desde la rama "main"...
-git push eternal main
-echo Proceso completado.
